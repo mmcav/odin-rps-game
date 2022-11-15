a4619b715725c4edde287ed1fba0dbc95a15cb4a
Marcelo <19808040+mmcav@users.noreply.github.com>
2022-11-15 11:27:15 -0300

    Changed input and output to document itself Edited index.html and script.js. Now user input is based on document buttons, and output is in the form of document text. No more need for the console log.

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-const rockPaperScissors = ["rock", "paper", "scissors"];
+gameStart();
 
-function game() {
+function gameStart() {
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection;
-        let message = "Rock, paper or scissors?";
-        let firstRun = true;
-        while (true) {
-            playerSelection = prompt(message).toLowerCase();
-            if (rockPaperScissors.includes(playerSelection)) {
-                break;
-            } else if (firstRun) {
-                message = "Invalid input!\n" + message;
-                firstRun = false;
-            }
-        }
-        const computerSelection = getComputerChoice();
-        const resultCode = playRound(playerSelection, computerSelection); 
-        let resultPhrase;
-        if (resultCode === 1) {
-            resultPhrase = `Tie! Both picked ${playerSelection}.`;
-        } else if (resultCode === 2) {
-            resultPhrase = `You win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`;
-            playerScore++;
-        } else if (resultCode === 3) {
-            resultPhrase = `You lose! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`;
-            computerScore++;
-        }
-        console.log(resultPhrase);
-        console.log(`Current score: ${playerScore} x ${computerScore}`);
+    const playerChoices = document.querySelectorAll('button');
+    playerChoices.forEach((playerChoice) => {
+        playerChoice.addEventListener('click', () => {
+            [playerScore, computerScore] = game(playerChoice.value, playerScore, computerScore);
+        });
+    });
+}
+
+function game(playerSelection, playerScore, computerScore) {
+    const computerSelection = getComputerChoice();
+    const resultCode = playRound(playerSelection, computerSelection); 
+    let resultPhrase;
+    if (resultCode === 1) {
+        resultPhrase = `Tie! Both picked ${playerSelection}.`;
+    } else if (resultCode === 2) {
+        resultPhrase = `You win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`;
+        playerScore++;
+    } else if (resultCode === 3) {
+        resultPhrase = `You lose! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`;
+        computerScore++;
     }
-    if (playerScore > computerScore) {
-        console.log("You won the game!");
-    } else if (playerScore < computerScore) {
-        console.log("You lost the game!");
-    } else {
-        console.log("Tie!")
-    }
+    resultPhrase += `\nCurrent score: ${playerScore} x ${computerScore}`
+    const resultContainer = document.querySelector('#results');
+    const result = document.createElement('p');
+    result.textContent = resultPhrase;
+    resultContainer.appendChild(result);
+    return [playerScore, computerScore];
 }
 
 function getComputerChoice() {
+    const rockPaperScissors = ["rock", "paper", "scissors"];
     return rockPaperScissors[Math.floor(Math.random() * 3)];
 }
 
@@ -48,13 +41,10 @@ function playRound(player, computer) {
     if (player === computer) {
         return 1;
     } else {
-        const win = (player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper');
-        if (win) {
+        if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper')) {
             return 2;
         } else {
             return 3;
         }
     }
 }
-
-game();
